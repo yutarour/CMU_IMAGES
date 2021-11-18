421949d06166256b31b50c8b13640496d38f60db
Yaurrdautia <63754863+Yaurrdautia@users.noreply.github.com>
2021-11-17 20:51:44 -0800

    Create allurls.py

---
+++ sdglogos/allurls.py
@@ -0,0 +1,26 @@
+#good place to get urls that work.
+#there is a list that you can copy paste into your code to use it.
+
+#0. transparent web wheel: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/SDG%20Wheel_Transparent_WEB.png
+#1. white background web wheel no logo: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/SDG%20Wheel_WEB.png
+#2. SDG #1: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%201.png
+#3. SDG #2: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%202.png
+#4. SDG #3: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%203.png
+#5. SDG #4: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%204.png
+#6. SDG #5: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%205.png
+#7. SDG #6: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%206.png
+#8. SDG #7: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%207.png
+#9. SDG #8: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%208.png
+#10. SDG #9: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%209.png
+#11. SDG #10: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2010.png
+#12. SDG #11: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2011.png
+#13. SDG #12: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2012.png
+#14. SDG #13: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2013.png
+#15. SDG #14: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2014.png
+#16. SDG #15: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2015.png
+#17. SDG #16: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2016.png
+#18. SDG #17: https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2017.png
+
+imagelist = ["https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/SDG%20Wheel_Transparent_WEB.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/SDG%20Wheel_WEB.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%201.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%201.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%203.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%204.png"
+,"https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%205.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%206.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%207.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%208.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%209.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2010.png"
+"https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2011.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2012.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2013.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2014.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2015.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2016.png","https://raw.githubusercontent.com/Yaurrdautia/CMU_IMAGES/main/sdglogos/unsdg%2017.png"]
